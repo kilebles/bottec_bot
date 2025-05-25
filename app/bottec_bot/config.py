bde0182e148f67811e85a5fc02c7efb3420e5179
Elisey <eliseyblajen@gmail.com>
2025-05-26 00:14:20 +0500

    Add postgres container

--- app/bottec_bot/config.py
+++ app/bottec_bot/config.py
@@ -11,6 +11,12 @@ class Settings(BaseSettings):
     GROUP_LINK: str
     GROUP_ID: int
     
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    
     model_config = SettingsConfigDict(env_file='.env')
     
 
