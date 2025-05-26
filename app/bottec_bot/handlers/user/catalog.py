9a3f888ad4f98b224b67496ce39e77244697ed5c
Elisey <eliseyblajen@gmail.com>
2025-05-26 23:47:12 +0500

    Add blockqoute

--- app/bottec_bot/handlers/user/catalog.py
+++ app/bottec_bot/handlers/user/catalog.py
@@ -85,7 +85,7 @@ async def show_product_detail(callback: CallbackQuery):
 
     text = (
         f'<b>{product.title}</b>\n\n'
-        f'<blockquote expandable>{product.description or "Без описания"}<blockquote>\n\n'
+        f'<blockquote expandable>{product.description or "Без описания"}</blockquote>\n\n'
         f'<b>Цена:</b> {product.price}₽'
     )
     
