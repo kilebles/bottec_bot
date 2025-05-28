14f89916de60739b2f8f0b8c74bc601c8b30cdaa
Elisey <eliseyblajen@gmail.com>
2025-05-28 06:27:40 +0500

    Update path catalog admin

--- app/admin/catalog_admin/apps.py
+++ app/admin/catalog_admin/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CatalogAdminConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "catalog_admin"
+    name = "app.admin.catalog_admin" 
